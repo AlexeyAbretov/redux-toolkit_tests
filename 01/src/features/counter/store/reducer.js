fada0bd5b4e5f887dcafd906ba2be518e241e232
Alexey Abretov <alexey.abretov@gmail.com>
2021-04-26 17:22:22 +0300

    feat(immer): added immer example

--- 01/src/features/counter/store/reducer.js
+++ 01/src/features/counter/store/reducer.js
@@ -4,6 +4,7 @@ export const counterSlice = createSlice({
   name: 'counter',
   initialState: {
     value: 0,
+    values: [0],
   },
   reducers: {
     increment: (state) => {
@@ -11,13 +12,16 @@ export const counterSlice = createSlice({
       // doesn't actually mutate the state because it uses the Immer library,
       // which detects changes to a "draft state" and produces a brand new
       // immutable state based off those changes
-      state.value += 1
+      state.value += 1;
+
+      state.values.push(state.value + 1);
     },
     decrement: (state) => {
-      state.value -= 1
+      state.values = state.values.filter((x) => x !== state.value);
+      state.value -= 1;
     },
     incrementByAmount: (state, action) => {
-      state.value += action.payload
+      state.value += action.payload;
     },
   },
 })
